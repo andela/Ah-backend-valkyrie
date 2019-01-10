1e31804830b563fb6f3bd20827032e6538c9b79a
michael <awesomeme155@gmail.com>
2019-01-10 11:11:55 +0300

    Chore(testing): Write unit tests for all functionality * Change script setting from nosetets to python manage.py test * Add view name parameter to the path() in authentication urls file * Add and set the TEST_RUNNER property to django_nose * Remove the namespace keyword argument from project urls file * Add a tests package to authentication apps * Install django-nose package
    [DELIVERS #162633484]
    (https://www.pivotaltracker.com/story/show/162633484)

---
+++ authors/apps/authentication/tests/base.py
@@ -0,0 +1,37 @@
+from rest_framework.test import APITestCase
+from django.urls import reverse
+
+
+class BaseTestMethods(APITestCase):
+   
+    def setUp(self):
+        self.user = {
+            'user':{
+            'username':'testuser', 
+            'email':'testuser@andela.com' , 
+            'password':'TestUser12#'
+            }
+        }
+
+    # User registration and login helper methods
+    def register_user(self):
+        url = reverse('user-registration')
+        data = {'user': {'username': self.user['user']['username'], 
+        'email': self.user['user']['email'], 
+        'password': self.user['user']['password']
+        }}
+
+        response = self.client.post(url, data=data, format='json')
+
+        return response
+
+    def register_and_loginUser(self):
+        _ = self.register_user()
+
+        url = reverse('user-login')
+        data = {'user': {'email': self.user['user']['email'], 'password': self.user['user']['password']}}
+        response = self.client.post(url, data=data, format='json')
+
+        return response
+
+
---
+++ authors/apps/authentication/tests/test_user_login.py
@@ -0,0 +1,87 @@
+from rest_framework import status
+from rest_framework.reverse import reverse
+
+from authors.apps.authentication.models import User
+from authors.apps.authentication.tests.base import BaseTestMethods
+
+
+class TestUserRegistration(BaseTestMethods):
+    """
+    Test cases for user registration and login
+    """
+    
+    def test_ordinary_user(self):
+        user = User.objects.create_user(**self.user['user'])
+       
+        self.assertEqual(user.__str__(), self.user['user']['email'])
+        self.assertEqual(user.get_full_name, self.user['user']['username'])
+        self.assertEqual(user.get_short_name(), self.user['user']['username'])
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+
+    def test_create_super_user(self):
+        super_user = User.objects.create_superuser(**self.user['user'])
+
+        self.assertTrue(super_user.is_superuser)
+        self.assertTrue(super_user.is_staff)
+    
+    def test_successful_user_registration(self):
+        response = self.register_user()
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data, {'email': self.user['user']['email'], 'username': self.user['user']['username']})
+        self.assertIsInstance(response.data, dict)
+
+    def test_successful_user_login(self):
+        response = self.register_and_loginUser()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {'email': self.user['user']['email'], 'username': self.user['user']['username']})
+        
+
+    def test_login_with_un_matching_email(self):
+
+        _ = self.register_user()
+
+        url = reverse('user-login')
+        data = {'user': {'email': 'invalid@test.com', 'password': self.user['user']['password']}}
+        response = self.client.post(url, data=data, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('errors', response.data)
+        self.assertIn('A user with this email and password was not found.', response.data['errors']['error'][0])
+
+    def test_login_without_an_email(self):
+
+        _ = self.register_user()
+        
+        url = reverse('user-login')
+        data = {'user': {'password': self.user['user']['password']}}
+        response = self.client.post(url, data=data, format='json')
+       
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('This field is required.', response.data['errors']['email'][0])
+
+    def test_login_without_a_password(self):
+        _ = self.register_user()
+        
+        url = reverse('user-login')
+        data = {'user': {'email': self.user['user']['email']}}
+        response = self.client.post(url, data=data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('This field is required.', response.data['errors']['password'][0])
+
+
+    
+
+
+    
+
+
+    
+
+
+        
+
+
