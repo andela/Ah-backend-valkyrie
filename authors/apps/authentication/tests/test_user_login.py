b0a4e303b755c22db2cb293f5b439a1410acfff7
Baker Sekitoleko <b.alzawad@gmail.com>
2019-01-14 18:32:38 +0300

    fix(Travis errors): Travis build failure This commit adds SECRET_KEY environment variable to the travis.yml file. It also fixes the failing tests as a result of modifications made to the code.
    [Fixes #162633496]

--- authors/apps/authentication/tests/test_user_login.py
+++ authors/apps/authentication/tests/test_user_login.py
@@ -9,10 +9,10 @@ class TestUserRegistration(BaseTestMethods):
     """
     Test cases for user registration and login
     """
-    
+
     def test_ordinary_user(self):
         user = User.objects.create_user(**self.user['user'])
-       
+
         self.assertEqual(user.__str__(), self.user['user']['email'])
         self.assertEqual(user.get_full_name, self.user['user']['username'])
         self.assertEqual(user.get_short_name(), self.user['user']['username'])
@@ -24,17 +24,14 @@ class TestUserRegistration(BaseTestMethods):
 
         self.assertTrue(super_user.is_superuser)
         self.assertTrue(super_user.is_staff)
-    
+
     def test_successful_user_registration(self):
         response = self.register_user()
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(
-            response.data, {
-                'email': self.user['user']['email'], 
-                'username': self.user['user']['username']
-                }
-            )
+            response.data.get('email'), self.user['user']['email']
+        )
         self.assertIsInstance(response.data, dict)
 
     def test_successful_user_login(self):
@@ -42,12 +39,8 @@ class TestUserRegistration(BaseTestMethods):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(
-            response.data, {
-                'email': self.user['user']['email'],
-                'username': self.user['user']['username']
-            }
+            response.data.get('email'), self.user['user']['email']
         )
-        
 
     def test_login_with_un_matching_email(self):
 
@@ -55,42 +48,42 @@ class TestUserRegistration(BaseTestMethods):
 
         url = reverse('user-login')
         data = {'user': {
-            'email': 'invalid@test.com', 
+            'email': 'invalid@test.com',
             'password': self.user['user']['password']
-            }
+        }
         }
         response = self.client.post(url, data=data, format='json')
-        
+
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn('errors', response.data)
         self.assertIn(
             'A user with this email and password was not found.',
             response.data['errors']['error'][0]
-            )
+        )
 
     def test_login_without_an_email(self):
 
         self.register_user()
-        
+
         url = reverse('user-login')
         data = {'user': {'password': self.user['user']['password']}}
         response = self.client.post(url, data=data, format='json')
-       
+
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn(
-            'This field is required.', 
+            'This field is required.',
             response.data['errors']['email'][0]
-            )
+        )
 
     def test_login_without_a_password(self):
         self.register_user()
-        
+
         url = reverse('user-login')
         data = {'user': {'email': self.user['user']['email']}}
         response = self.client.post(url, data=data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn(
-            'This field is required.', 
+            'This field is required.',
             response.data['errors']['password'][0]
-            )
+        )
