fd4d900bb4b153db5354d9f677da22e6d0cfbfd5
Timothy Mubiru <mubstimor@gmail.com>
2019-01-16 20:06:42 +0300

    feat(articles): set up test class
    add test methods for testing article functionality
    
    [Delivers #162633502]

--- authors/apps/articles/tests.py
+++ authors/apps/articles/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
 
-# Create your tests here.
+from articles.models import Article, Tag
+
+class ArticleTestCase(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_user_creates_article(self):
+        pass
+
+    def test_user_with_incomplete_info(self):
+        pass
+
+    def test_create_article_with_duplicate_title(self):
+        pass
+
+    def test_update_article_by_author(self):
+        pass
+
+    def test_update_article_by_invalid_author(self):
+        pass
+
+    def test_delete_article_by_author(self):
+        pass
+
+    def test_delete_article_by_invalid_author(self):
+        pass
