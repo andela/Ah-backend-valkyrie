c2a3738152c53775c83f129bb7bcecb45a9ef027
araalifarooq <farooqp35h@gmail.com>
2019-01-21 12:50:13 +0300

    #162633500-ft(Social Login): Implements social login.
    This commit implement user login via twitter.
    [Delivers #162633500]

--- authors/apps/authentication/social_auth/serializers.py
+++ authors/apps/authentication/social_auth/serializers.py
@@ -1,18 +1,17 @@
 from rest_framework import serializers
-from .service_providers import ServiceProviders, TwitterAuthTokenVerification
+from .service_providers import ServiceProviders
 from .register import register_user
 import facebook
 from django.http import JsonResponse
 
 
 class FacebookSerializer(serializers.Serializer):
-    # serialization of facebook related data
+    # serialization of facebook data
 
     auth_token = serializers.CharField()
 
     def validate_auth_token(self, access_token):
         my_facebook = ServiceProviders()
-        # try:
         user_info = my_facebook.verify(
             provider="facebook", token=access_token
         )
@@ -35,7 +34,7 @@ class FacebookSerializer(serializers.Serializer):
 
 
 class GoogleSerializer(serializers.Serializer):
-    # serialization of google related data.
+    # serialization of google data.
 
     auth_token = serializers.CharField()
 
@@ -60,26 +59,25 @@ class GoogleSerializer(serializers.Serializer):
 
 
 class TwitterSerializer(serializers.Serializer):
-    """Handles serialization of twitter related data"""
+    # serialization of twitter data
 
     auth_token = serializers.CharField()
 
     def validate_auth_token(self, auth_token):
-
-        user_info = TwitterAuthTokenVerification.validate_twitter_auth_tokens(
-            auth_token)
+        twitter = ServiceProviders()
+        user_info = twitter.verify(provider="twitter", token=auth_token)
         try:
             user_info['id_str']
         except:
             raise serializers.ValidationError(
-                'The token is invalid or expired. Please login again.'
+                'Invalid or expired token. Please login again'
             )
-
         user_id = user_info['id_str']
         email = user_info['email']
         name = user_info['name']
         my_kwargs = {
             '{0}_{1}'.format('twitter', 'id'): user_id
         }
-
-        return register_user(email=email, name=name, **my_kwargs)
+        return register_user(
+            email=email, name=name, **my_kwargs
+        )
--- authors/apps/authentication/tests/test_social_auth.py
+++ authors/apps/authentication/tests/test_social_auth.py
@@ -41,4 +41,23 @@ class TestSocialAuth(BaseTestMethods):
                 'Invalid or expired token. Please login again.', str(
                     response.data))        
 
-                                           
+    def test_login_with_invalid_twitter_token(self):
+        # user login with invalid google token
+        response = self.client.post(
+            "/api/v1/auth/twitter/", {"user": {
+                "auth_token": self.invalid_twitter_token}}, format='json')      
+        self.assertEqual(
+            response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn(
+                'Invalid or expired token. Please login again', str(
+                    response.data)
+        )
+
+    def test_login_with_valid_twitter_token(self):
+        # user login with invalid google token
+        response = self.client.post(
+            "/api/v1/auth/twitter/", {"user": {
+                "auth_token": self.valid_twitter_token}}, format='json')      
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK)
+            
