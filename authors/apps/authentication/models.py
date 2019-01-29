848b3ddf16ae1a2a1b9a9dab13db0c50238867a8
manzede benard <manzede@gmail.com>
2019-01-29 21:49:39 +0300

    bg Fix bugs and ignore migrations folder
    * [Fx] Add migration for travis
    
    * Add auth user table
    
    * [Fx] Add make migrations for travis
    
    * add back migrations folder
    
    * make is_active to False

--- authors/apps/authentication/models.py
+++ authors/apps/authentication/models.py
@@ -114,6 +114,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     class Meta:
         managed = True
         abstract = False
+        db_table = 'auth_user'
 
     def __str__(self):
         """
