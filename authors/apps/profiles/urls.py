acce81366d1687efdedcfef01ff7470ebe94cd66
Frank Atukunda <fatukunda@hotmail.com>
2019-01-31 09:33:22 +0300

    feat(User following) - Users should follow each other
    * Feature(Profile Following)-Users should follow each other
    
    * Modify the Profiles app to include this feature
    * Modify the profile models, serializers, urls, views, and renderers to cater for this feature
    * Write unit tests for this feature
    
    * feat: tag articles
    
    * Enable users to tag articles
    
    [Delivers #162633502]

--- authors/apps/profiles/urls.py
+++ authors/apps/profiles/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 
-from .views import ProfileRetrieveAPIView
+from .views import (ProfileRetrieveAPIView, ProfileFollowAPIView,
+                    ProfileFollowersAPIView, ProfileFollowingAPIView,)
 
 urlpatterns = [
     path('users/<int:id>/profile/',
          ProfileRetrieveAPIView.as_view(), name='profile'),
+    path('users/<username>/profile/follow',
+         ProfileFollowAPIView.as_view(), name='follow-profile'),
+    path('users/me/profile/followers',
+         ProfileFollowersAPIView.as_view(), name='profile-followers'),
+    path('users/me/profile/followings',
+         ProfileFollowingAPIView.as_view(), name='followings')
 ]
